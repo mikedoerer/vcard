d04fb1eeeb6537cc6155135ee6ba6992c7780ae2
Michael Dörer <michael.doerer@multivisio.de>
2023-10-27 22:58:28 +0200

    EsLint and Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
   buildModules: ["@nuxtjs/style-resources"],
   alias: {
-    'module': fileURLToPath(new URL('./scss', import.meta.url))
-  },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: ``,
-        },
-      },
-    },
+    module: fileURLToPath(new URL("./scss", import.meta.url)),
   },
 });
