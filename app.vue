dec8547e20585602576c33f026d650057dccfc4c
Michael Dörer <michael.doerer@multivisio.de>
2023-10-27 17:30:27 +0200

    Init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+
+    <ul>
+        <li><NuxtLink href="/">Home</NuxtLink></li>
+        <li><NuxtLink href="/qr">QR</NuxtLink></li>
+    </ul>
+
+</header>
+<main>
+    <NuxtPage page-key="static" />
+</main>
+<footer>
+
+</footer>
+</template>
+<script setup lang="ts">
+</script>
